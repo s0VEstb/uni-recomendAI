3cc161ad900dd510e3f1df18b4228ab1bf24b549
Bektur Ibraimov <bekturibraimov21@gmail.com>
2026-02-21 20:44:59 +0600

    Changed port for frontend

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def create_app() -> FastAPI:
     )
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+        allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
